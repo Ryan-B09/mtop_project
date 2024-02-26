87973105f6fa45976f8ef2dae26652071e16d678
Codux <codux@codux.com>
2024-02-26 11:50:04 +0800

    updates

--- src/_codux/boards/loginpage.board.tsx
+++ src/_codux/boards/loginpage.board.tsx
@@ -9,7 +9,9 @@ export default createBoard({
     name: 'loginpage',
     Board: () => (
         <div className="root">
-            <h1 className="title">MTOP San Pablo City</h1>
+            <h1 className="title font-serif drop-shadow-xl">
+                MTOP San Pablo City
+            </h1>
             <div className="header bg-white-600">
                 <img src={userlogo} alt="" className="user" />
                 <img src={spclogo} alt="" className="SpcLogo" />
@@ -19,31 +21,30 @@ export default createBoard({
                 <form className="newlog">
                     <input
                         type="text"
-                        className="email"
-                        placeholder="        E-Mail"
+                        className="email border-2 hover:border-kolor-1 text-stone-950 hover:text-kolor-1 bg-kolor-2/20"
+                        placeholder="E-Mail"
                     />
                     <br />
                     <br />
                     <input
-                        type="text"
-                        className="pass"
-                        placeholder="        Password"
+                        type="password"
+                        className="pass border-2 hover:border-kolor-1  text-stone-950 hover:text-kolor-1 bg-kolor-2/20"
+                        placeholder="Password"
                     />
                     <br />
-                    <input   
+                    <input
                         type="submit"
                         value="LOG IN"
                         className="login bg-kolor-1 text-white hover:bg-sky-900 cursor-pointer"
                     />
                     <input
-                        type="submit" 
+                        type="submit"
                         value="Forgot Password?"
                         className="forgot text-gray-400 underline hover:decoration-2 hover:decoration-kolor-1 cursor-pointer"
                     />
                 </form>
             </div>
-        </div> 
-        
+        </div>
     ),
 
     isSnippet: true,
