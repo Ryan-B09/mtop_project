b3e072f52569b03caa430f71bac9eeefd879dd3e
Codux <codux@codux.com>
2024-02-26 10:00:22 +0800

    log in page trial

---
+++ src/_codux/boards/loginpage.board.tsx
@@ -0,0 +1,55 @@
+import '../../reset.css';
+import { createBoard } from '@wixc3/react-board';
+import MtopLogo from '../../assets/transport_logo_Light.svg';
+import spclogo from '../../assets/spc.png';
+import userlogo from '../../assets/user_light.svg';
+import pass from '../../assets/password_light.svg';
+
+export default createBoard({
+    name: 'loginpage',
+    Board: () => (
+        <div className="root">
+            <h1 className="title">MTOP San Pablo City</h1>
+            <div className="header bg-white-600">
+                <img src={userlogo} alt="" className="user" />
+                <img src={spclogo} alt="" className="SpcLogo" />
+                <img src={MtopLogo} alt="" className="mtoplogo" />
+                <img src={pass} alt="" className="word" />
+                <div className="V-line" />
+                <form className="newlog">
+                    <input
+                        type="text"
+                        className="email"
+                        placeholder="        E-Mail"
+                    />
+                    <br />
+                    <br />
+                    <input
+                        type="text"
+                        className="pass"
+                        placeholder="        Password"
+                    />
+                    <br />
+                    <input   
+                        type="submit"
+                        value="LOG IN"
+                        className="login bg-kolor-1 text-white hover:bg-sky-900 cursor-pointer"
+                    />
+                    <input
+                        type="submit" 
+                        value="Forgot Password?"
+                        className="forgot text-gray-400 underline hover:decoration-2 hover:decoration-kolor-1 cursor-pointer"
+                    />
+                </form>
+            </div>
+        </div> 
+        
+    ),
+
+    isSnippet: true,
+    environmentProps: {
+        windowHeight: 768,
+        windowWidth: 1024,
+        canvasWidth: 500,
+    },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,20 @@ export default {
   theme: {
     extend: {
       colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
         blue: "#495CEF",
         yellow: "#FFC328",
         "gray-dark": "#8D8D8D",
         "gray-med": "#D9D9D9",
         "gray-light": "#F3F3F3",
+
+
+//SYSTEM COLOR PALETTE
+        'kolor': {
+          1: '#0B60B0',
+          2: '#6B7280',
+        }
       },
     },
   },
